8b7380a9bf95aa863566c33bc60552c9761cfab9
JACQUES sandra <ssephiha1905@gmail.com>
2022-02-02 19:23:26 +0100

    coursp6

--- app.js
+++ app.js
@@ -36,6 +36,16 @@ app.use((req, res, next) => {
       .then(() => res.status(201).json({ message: 'Objet enregistré !'}))
       .catch(error => res.status(400).json({ error }));
   });
+  app.put('/api/stuff/:id', (req, res, next) => {
+    Thing.updateOne({ _id: req.params.id }, { ...req.body, _id: req.params.id })
+      .then(() => res.status(200).json({ message: 'Objet modifié !'}))
+      .catch(error => res.status(400).json({ error }));
+  });
+  app.delete('/api/stuff/:id', (req, res, next) => {
+    Thing.deleteOne({ _id: req.params.id })
+      .then(() => res.status(200).json({ message: 'Objet supprimé !'}))
+      .catch(error => res.status(400).json({ error }));
+  });
 
 module.exports = app;
 
